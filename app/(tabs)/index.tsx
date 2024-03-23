e746289467f914602137904a77ebfdd64091be39
Jepjep <jeffreyechano@gmail.com>
2024-03-23 22:45:56 +0800

    test branch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,8 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <Text className='text-5xl'>asd</Text>
+      <Text className='text-5xl'>asdasdaasd</Text>
+      <Text>testtt</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
